8e69b43a502f83ea482ce4034e80c70538a3e587
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-12-10 06:16:39 +0200

    add and style background decorations

--- farm-project/src/main.tsx
+++ farm-project/src/main.tsx
@@ -4,7 +4,19 @@ export function Main() {
 
   return (
     <>
+      <div className="main_container">
+        <div className="text_container">
+          <h1>
+            Submit Your Letter to Santa!
+          </h1>
+          <p>Just hopping you've been good this year</p>
+        </div>
 
+        <div className="background_forms">
+          <img src="./tree.svg" alt="tree" className="tree_decoration" />
+          <img src="./ho-ho-ho.svg" alt="ho-ho-ho" className="hohoho_decoration" />
+        </div>
+      </div>
     </>
   );
 }
